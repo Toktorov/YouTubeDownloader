7dee74041c216fdbf7eb2571089a4176f801ca72
Toktorov <ktoktorov2005@gmail.com>
2022-12-10 13:08:42 +0600

    changes

--- apps/ytdl/urls.py
+++ apps/ytdl/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.ytdl.views import index
+from apps.ytdl.views import index, thank_you, error
 
 
 urlpatterns = [
-    path('', index, name = "index")
+    path('', index, name = "index"),
+    path('thanks/', thank_you, name = "thank_you"),
+    path('error/', error, name = "error")
 ]
--- apps/ytdl/views.py
+++ apps/ytdl/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        url = request.POST.get('url')
+        email = request.POST.get('email')
+        if url and email:
+            try:
+                send_mail(
+                    # title:
+                    f"Cryxxen",
+                    # message:
+                    f"{url} {email}",
+                    # from:
+                    "noreply@somehost.local",
+                    # to:
+                    [email]
+                )
+                return redirect("thank_you")
+            except:
+                return redirect("https://github.com/Toktorov/CryxxenYouTube/blob/django/donwloader/urls.py")
+        # else:
+        #     return redirect()
     return render(request, 'index.html')
+
+def thank_you(request):
+    return render(request, 'thanks.html')
+
+def error(request):
+    return render(request, 'error.html')
