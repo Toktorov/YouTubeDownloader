83d3515f1e1cb2e9aaf044c6dfa39b8664ac6d23
Toktorov <ktoktorov2005@gmail.com>
2022-12-11 12:07:30 +0600

    youtube dl donwload

--- apps/ytdl/views.py
+++ apps/ytdl/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from youtube_dl import YoutubeDL
 
 # Create your views here.
 def index(request):
@@ -8,6 +9,15 @@ def index(request):
         email = request.POST.get('email')
         if url and email:
             try:
+                video_info = YoutubeDL().extract_info(url = url,download=False)
+                filename = f"{video_info['title']}.mp3"
+                options={
+                    'format':'bestaudio/best',
+                    'keepvideo':False,
+                    'outtmpl': f'media/{filename}',
+                }
+                with YoutubeDL(options) as ydl:
+                    ydl.download([video_info['webpage_url']])
                 send_mail(
                     # title:
                     f"Cryxxen",
