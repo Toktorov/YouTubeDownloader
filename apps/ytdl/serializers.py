78947aa956db856e4b5a01e9af07cedc337a205c
Toktorov <ktoktorov2005@gmail.com>
2022-12-11 22:58:39 +0600

    full

--- apps/ytdl/serializers.py
+++ apps/ytdl/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from apps.ytdl.tasks import send_feedback_email_task
 
+
+#Создаем сериализатор для получения данных через API
 class MailSerializer(serializers.Serializer):
     url = serializers.CharField(
         label="URL"
     )
     email = serializers.EmailField(label="Email Address")
 
-    def send_email(self):
+    def send_email(self): #Вызываем task
         send_feedback_email_task.delay(
             self.initial_data['url'],
             self.initial_data['email']
