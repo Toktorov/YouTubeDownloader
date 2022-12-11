7c6555718ecb3688fc0aea397e98aaae9de1fe5a
Toktorov <ktoktorov2005@gmail.com>
2022-12-11 20:07:19 +0600

    redis celery ready 100%

--- apps/ytdl/forms.py
+++ apps/ytdl/forms.py
@@ -7,11 +7,7 @@ class MailForm(forms.Form):
         label="URL", widget=forms.Textarea(attrs={"rows": 1})
     )
     email = forms.EmailField(label="Email Address")
-    widgets = {
-        'url': forms.Textarea(attrs={'class': "form-control"}),
-        'email': forms.Textarea(attrs={'class': 'form-control'}),
-    }
-    
+  
     def send_email(self):
         send_feedback_email_task.apply_async(args=[
             self.cleaned_data["url"], self.cleaned_data["email"]
